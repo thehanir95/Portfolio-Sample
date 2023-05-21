c385b49486a3427c5713f9149dd6e81f003fdbea
thehanir95 <thehanicis95@gmail.com>
2023-05-21 07:13:59 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,11 @@
 import React from 'react'
-import Header from './components/Header/Header'
-import Nav from './components/Nav/Nav'
-import About from './components/About/About'
-import Experiance from './components/Experiance/Experiance'
-import Services from './components/Services/Services'
 import Portfolio from './components/Portfolio/Portfolio'
-import Testimonials from './components/Testimonials/Testimonials'
-import Contact from './components/Contact/Contact'
-import Footer from './components/Footer/Footer'
 
 
 const App = () => {
   return (
     <>
-        <Header />
-        <Nav/>
-        <About />
-        <Experiance />
-        <Services />
         <Portfolio />
-        <Testimonials />
-        <Contact />
-        <Footer />
     </>
   )
 }
